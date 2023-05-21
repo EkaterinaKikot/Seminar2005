118cd39306cc4696134db1e5eb2ea1b14819d4d6
Kikot Ekaterina <kati-kik@yandex.ru>
2023-05-21 18:19:44 +0300

    решена задача 2

---
+++ Task2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Введите 1-ое число: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.Write("Введите 2-ое число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+if (n > m)
+Console.WriteLine("Первое число больше второго. Второе число меньше первого");
+else if (n < m)
+Console.WriteLine("Второе число больше первого. Первое число меньше второго");
+else
+Console.WriteLine("Числа равны");
