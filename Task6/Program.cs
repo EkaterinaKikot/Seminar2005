e567401bbc1ee5ae48b8ea459bf63d66a743623b
Kikot Ekaterina <kati-kik@yandex.ru>
2023-05-21 20:44:33 +0300

    Решена 6 задача

---
+++ Task6/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.Write("Введите 1 число: ");
+int n = int.Parse(Console.ReadLine()!);
+if (n % 2 == 0)
+Console.WriteLine("Да. Чётное");
+else
+Console.WriteLine("Нет. Нечётное");
+
+
