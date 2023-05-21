b98d803edfc48a16e8f9a5a09a8908f20da564c6
Kikot Ekaterina <kati-kik@yandex.ru>
2023-05-21 19:18:14 +0300

    Решена 4 задача

---
+++ Task4/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.WriteLine("Введите 1 число: ");
+int a = Convert.ToInt32(Console.ReadLine()!);
+Console.WriteLine("Введите 2 число: ");
+int b = Convert.ToInt32(Console.ReadLine()!);
+Console.WriteLine("Введите 3 число: ");
+int c = Convert.ToInt32(Console.ReadLine()!);
+int max = a;
+if (max < b) max = b;
+if (max < c) max = c;
+Console.WriteLine("Максимальное из этих чисел ="+ max);
+
